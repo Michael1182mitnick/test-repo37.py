f60350cb870a1b8b8369d5f5ac6355fe88b95579
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-07-26 22:33:30 -0800

    Create test-repo37.py

---
+++ test-repo37.py
@@ -0,0 +1,7 @@
+# Write a Python program to reverse a given string.
+
+new_list_of_string = input("Enter any word: ")
+
+# To reverse a string 
+new_string = new_list_of_string[::-1]
+print(new_string)
